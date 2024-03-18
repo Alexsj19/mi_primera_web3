9e1dc97379c420d7b51df631e7c8b85b362165c8
Alexsj19 <alexsoller05@gmail.com>
2024-03-17 19:55:20 -0500

    Initial commit

---
+++ scrip.js
@@ -0,0 +1,46 @@
+//alert("Hola este es mi Javascript");
+
+//let contenidoTitulo = "alex";
+
+//let titulo = document.querySelector(".logo .fuente-acento");
+//titulo.innerHTML = contenidoTitulo;
+
+//condicionales
+//let textotitulo = titulo.innerText;
+//console.log(textotitulo);
+
+//if (contenidoTitulo== "nombre") { titulo.innerHTML = "otro"; }
+
+//else { console.log(" no se cumple");}
+
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+  if (!valido) {
+    parrafo.innerHTML = warnings;
+  } else {
+    parrafo.innerHTML = "Enviado";
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
+
